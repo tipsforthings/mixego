d821f31db81edbaf06810b8c27c97f8007baa822
Alex Scott <alex@alxs.co.uk>
2015-02-28 12:08:25 +0000

    Add account activation and password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
-      render 'new'
+      render 'new', layout: "frontpage"
     end
   end
 
